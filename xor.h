e407f851f761900e11ec10fb0eff2d5cd30926c2
Ventona666 <58355856+Ventona666@users.noreply.github.com>
2021-01-07 09:58:48 +0100

    Add files via upload

---
+++ xor.h
@@ -0,0 +1,10 @@
+//
+// Created by TONY DE FREITAS on 02/12/2020.
+//
+
+#ifndef PROJET_S3_XOR_H
+#define PROJET_S3_XOR_H
+
+#endif //PROJET_S3_XOR_H
+
+void cryptage(char* text_to_encode, char* text_encoded, char* key);
